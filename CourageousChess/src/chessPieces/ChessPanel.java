52db19906ffeefdbddf0c20eefed302f46b59c0c
rofel <rofel@rofel>
2014-11-27 11:01:16 -0500

    Made Grid resizeable

--- CourageousChess/src/chessPieces/ChessPanel.java
+++ CourageousChess/src/chessPieces/ChessPanel.java
@@ -11,40 +11,49 @@ public class ChessPanel extends JPanel{
 	private ChessPiece[][] chess;
 	private Rectangle [][] chessGrid;
 	private Rectangle square;
-
+	
 	private final int GRID = 8;//Chess Grid 8 by 8
-	private final int START_POINTX = 20;//Starting point of grid location
-	private final int START_POINTY = 20;
-	private final int GRID_SIZE = 90;
+	private final int START_POINTX = 0;//Starting point of grid location
+	private final int START_POINTY = 0;
+	private double END_POINTX = this.getHeight() / 8;
+	private double END_POINTY = this.getWidth() / 8;
 
-	private int locationX;
-	private int locationY;
+	private double locationX;
+	private double locationY;
 	private int colorGrid = 0;
-
+	
 	public ChessPanel(ChessPiece[][] chess) {
+		drawGrid();
+	}
+
+	public void drawGrid() {
 		this.chess = chess;
 		chessGrid = new Rectangle[GRID][GRID];
 		locationX = START_POINTX;
 		locationY = START_POINTY;
-
+		//END_POINTX = this.getWidth() / 8;
+		END_POINTY = this.getHeight() / 8;
+		
 		for(int i = 0; i< GRID; i++){
 			for(int j=0; j< GRID; j++){
-				square = new Rectangle(locationX,locationY,GRID_SIZE,GRID_SIZE);
+				square = new Rectangle((int)locationX,(int)locationY,(int)END_POINTY,(int)END_POINTY);
 				chessGrid[i][j] = square;
-				locationY = locationY + GRID_SIZE;
+				locationY = locationY + END_POINTY;
 			}
-			locationX = locationX + GRID_SIZE;
+			locationX = locationX + END_POINTY;
 			locationY = START_POINTY;
 		}
 	}
-
+	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		
-		for(int i =0; i<GRID; i++){
-			for(int j=0; j< GRID; j++){
-				if(colorGrid %2 ==0)
+		drawGrid();
+		
+		for(int i = 0; i <GRID; i++){
+			for(int j = 0; j < GRID; j++){
+				if(colorGrid % 2 == 0)
 					g2.setColor(Color.BLACK);
 				else
 					g2.setColor(Color.WHITE);
@@ -53,7 +62,9 @@ public class ChessPanel extends JPanel{
 			}
 			colorGrid++;
 		}
-		g2.drawString(ChessFrame.FRAME_WIDTH + " ", 100, 100);
+		
+		g2.fill(chessGrid[0][0]);
+		//g2.drawString(this.getHeight() + " " + this.getWidth(), 100, 100);
 	}
 
 }
