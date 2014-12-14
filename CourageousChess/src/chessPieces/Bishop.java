cb9c75a731edf35b2643c1a9e559638840a18f6d
Julius <jj_dsilva25@hotmail.com>
2014-12-14 16:33:43 -0500

    User can now move and capture pieces with the bishop

--- CourageousChess/src/chessPieces/Bishop.java
+++ CourageousChess/src/chessPieces/Bishop.java
@@ -1,16 +1,17 @@
 package chessPieces;
 
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.util.ArrayList;
 
 public class Bishop extends ChessPiece {
+	private int [] paths = {-1,-1, -1, 1, 1, -1, 1, 1};
+	private int nextX, nextY;
 
 	public Bishop(int x, int y, String name, String color, String gridColor) {
 		super(x,y, name, color, gridColor);
 		System.out.println(x + " " + y + " " + name + " " + color+" grid: " + gridColor);
 	}
-	
+
 	public void move() {
 
 	}
@@ -22,8 +23,32 @@ public class Bishop extends ChessPiece {
 	public void draw(Graphics2D g2, int gridSize) {
 		super.draw(g2, gridSize);
 	}
-	
+
 	public ArrayList<ChessAttributes> nextMoveSet(ArrayList<ChessAttributes> chess) {
+		this.setSelected(true);
+		for(int i = 0; i< paths.length; i+=2)
+			chess = nextMoveSet(chess,xPos,yPos,paths[i],paths[i+1]);
+
+		return chess;
+	}
+
+	public ArrayList<ChessAttributes> nextMoveSet(ArrayList<ChessAttributes> chess, int currentX, int currentY, int moveX, int moveY) {
+		nextX = currentX + moveX;
+		nextY = currentY + moveY;
+		System.out.println("I am here");
+		ChessAttributes piece = getChessPiece(chess,nextX, nextY);
+		if(nextX >= 0 && nextX <= 7 && nextY >= 0 && nextY <= 7){
+			if(this.getColor().equals(piece.getColor()))
+				return chess;
+			else if(!(this.getColor().equals(piece.getColor())) &&  !(piece.getColor().equals("Blank"))){
+				piece.setSelected(true);
+				return chess;
+			}
+			else{
+				piece.setSelected(true);
+				return nextMoveSet(chess,nextX,nextY,moveX,moveY);
+			}
+		}
 		return chess;
 	}
 
